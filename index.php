51c7408c5ee0f8bc272a494def38a56d9d363fc4
Sam <biryukwolf@yahoo.com>
2018-03-10 04:18:01 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,335 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Starter Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="catalog/view/javascript/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="catalog/view/javascript/bootstrap/dist/css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="libs/megamenu-js-master/megamenu-js-master/css/stylemega.css" rel="stylesheet">
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="catalog/view/javascript/bootstrap/docs/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/main.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="catalog/view/javascript/bootstrap/docs/assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="catalog/view/javascript/bootstrap/docs/assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+<!--naw bar top-->
+<nav class="navbar  navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand hidden-lg hidden-md" href="#">Eklectik</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">О нас</a></li>
+                <li><a href="#about">Услуги дизайна</a></li>
+                <li><a href="#contact">Советы и идеи</a></li>
+                <li><a href="#contact">Портфолио</a></li>
+                <li><a href="#contact">Новости</a></li>
+                <li><a href="#contact">Оплата и доставка</a></li>
+                <li><a href="#contact">Блог</a></li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</nav>
+<div class="clearfix"></div>
+<!--search -->
+<div class="container search">
+    <div class="col-md-6">
+        <img src="image/verstka/logo.png" alt="logo">
+    </div>
+    <div class="col-md-6">
+<div class="wrp_search_cart">
+    <div class="input-group">
+        <input type="text" class="form-control" placeholder="Search for...">
+        <span class="input-group-btn">
+        <button class="btn btn-default" type="button">Go!</button>
+      </span>
+    </div><!-- /input-group -->
+    <div class="cart">
+        <img src="image/verstka/cart.png" alt="cart">
+    </div>
+</div>
+    </div>
+</div>
+<div class="clearfix"></div>
+
+
+<!--naw bar bottom-->
+<!--mega menu-->
+<nav class="navbar navbar_bottom">
+<div class="container">
+    <div class="menu-container">
+        <div class="menu">
+            <ul>
+                <li class="active"><a href="#">New</a></li>
+                <li><a href="#">Изделия под заказ</a>
+                    <ul>
+                        <li><a href="http://marioloncarek.com">School</a>
+                            <ul>
+                                <li><a href="http://marioloncarek.com">Lidership</a></li>
+                                <li><a href="#">History</a></li>
+                                <li><a href="#">Locations</a></li>
+                                <li><a href="#">Careers</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Study</a>
+                            <ul>
+                                <li><a href="#">Undergraduate</a></li>
+                                <li><a href="#">Masters</a></li>
+                                <li><a href="#">International</a></li>
+                                <li><a href="#">Online</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Research</a>
+                            <ul>
+                                <li><a href="#">Undergraduate research</a></li>
+                                <li><a href="#">Masters research</a></li>
+                                <li><a href="#">Funding</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">Beauty image</a>
+                            <div class="mega_image">
+                            <img class="img-responsive" src="image/verstka/heap/images.jpg" alt="cat img">
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+                <li><a href="#">Винтаж</a>
+                    <ul>
+                        <li><a href="http://marioloncarek.com">School</a>
+                            <ul>
+                                <li><a href="http://marioloncarek.com">Lidership</a></li>
+                                <li><a href="#">History</a></li>
+                                <li><a href="#">Locations</a></li>
+                                <li><a href="#">Careers</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Study</a>
+                            <ul>
+                                <li><a href="#">Undergraduate</a></li>
+                                <li><a href="#">Masters</a></li>
+                                <li><a href="#">International</a></li>
+                                <li><a href="#">Online</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Research</a>
+                            <ul>
+                                <li><a href="#">Undergraduate research</a></li>
+                                <li><a href="#">Masters research</a></li>
+                                <li><a href="#">Funding</a></li>
+                            </ul>
+                        </li>
+
+                        <li><a href="#">Research</a>
+                            <ul>
+                                <li><a href="#">Undergraduate research</a></li>
+                                <li><a href="#">Masters research</a></li>
+                                <li><a href="#">Funding</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li><a href="#">Мебель</a>
+                    <ul>
+                        <li><a href="http://marioloncarek.com">School</a>
+                            <ul>
+                                <li><a href="http://marioloncarek.com">Lidership</a></li>
+                                <li><a href="#">History</a></li>
+                                <li><a href="#">Locations</a></li>
+                                <li><a href="#">Careers</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Study</a>
+                            <ul>
+                                <li><a href="#">Undergraduate</a></li>
+                                <li><a href="#">Masters</a></li>
+                                <li><a href="#">International</a></li>
+                                <li><a href="#">Online</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Research</a>
+                            <ul>
+                                <li><a href="#">Undergraduate research</a></li>
+                                <li><a href="#">Masters research</a></li>
+                                <li><a href="#">Funding</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">Beauty image</a>
+                            <div class="mega_image">
+                                <img class="img-responsive" src="image/verstka/heap/images.jpg" alt="cat img">
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+                <li><a href="#">Праздник</a></li>
+                <li><a href="#">Спальня & Ванная</a></li>
+                <li><a href="#">Столовая & Кухня</a></li>
+                <li><a href="#">Освещение</a></li>
+                <li><a href="#">Ковры & Шторы</a></li>
+                <li><a href="#">Декор & Разное</a></li>
+                <li><a href="#">Ткани</a></li>
+                <li><a href="#">Подарки</a></li>
+                <li><a class="sale" href="#">Sale</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+</nav>
+<div class="clearfix"></div>
+
+
+
+
+<!--slider-->
+<div class="container-fluid container-slider">
+    <!-- Slider start -->
+    <div id="first-slider">
+        <div id="carousel-example-generic" class="carousel slide carousel-fade">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+            </ol>
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+                <!-- Item 1 -->
+                <div class="item active slide1">
+                    <div class="row"><div class="container">
+                            <div class="col-md-3 text-right">
+                                <img style="max-width: 200px;"  data-animation="animated zoomInLeft" src="http://s20.postimg.org/pfmmo6qj1/window_domain.png">
+                            </div>
+                            <div class="col-md-9 text-left">
+                                <h3 data-animation="animated bounceInDown">Add images, or even your logo!</h3>
+                                <h4 data-animation="animated bounceInUp">Easily use stunning effects</h4>
+                            </div>
+                        </div></div>
+                </div>
+                <!-- Item 2 -->
+                <div class="item slide2">
+                    <div class="row"><div class="container">
+                            <div class="col-md-7 text-left">
+                                <h3 data-animation="animated bounceInDown"> 50 animation options A beautiful</h3>
+                                <h4 data-animation="animated bounceInUp">Create beautiful slideshows </h4>
+                            </div>
+                            <div class="col-md-5 text-right">
+                                <img style="max-width: 200px;"  data-animation="animated zoomInLeft" src="http://s20.postimg.org/sp11uneml/rack_server_unlock.png">
+                            </div>
+                        </div></div>
+                </div>
+                <!-- Item 3 -->
+                <div class="item slide3">
+                    <div class="row"><div class="container">
+                            <div class="col-md-7 text-left">
+                                <h3 data-animation="animated bounceInDown">Simple Bootstrap Carousel</h3>
+                                <h4 data-animation="animated bounceInUp">Bootstrap Image Carousel Slider with Animate.css</h4>
+                            </div>
+                            <div class="col-md-5 text-right">
+                                <img style="max-width: 200px;"  data-animation="animated zoomInLeft" src="http://s20.postimg.org/eq8xvxeq5/globe_network.png">
+                            </div>
+                        </div></div>
+                </div>
+                <!-- Item 4 -->
+                <div class="item slide4">
+                    <div class="row"><div class="container">
+                            <div class="col-md-7 text-left">
+                                <h3 data-animation="animated bounceInDown">We are creative</h3>
+                                <h4 data-animation="animated bounceInUp">Get start your next awesome project</h4>
+                            </div>
+                            <div class="col-md-5 text-right">
+                                <img style="max-width: 200px;"  data-animation="animated zoomInLeft" src="http://s20.postimg.org/9vf8xngel/internet_speed.png">
+                            </div>
+                        </div></div>
+                </div>
+                <!-- End Item 4 -->
+
+            </div>
+            <!-- End Wrapper for slides-->
+            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                <i class="fa fa-angle-left"></i><span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                <i class="fa fa-angle-right"></i><span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+    <!-- End of slider -->
+</div>
+<div class="clearfix"></div>
+
+
+<!--category-block-->
+<div class="container-fluid category-block">
+    <div class="container">
+        <?php foreach (range(1,10)as $k ){   ?>
+            <div class="simple_category">
+                <a href="#">название раздела</a>
+            </div>
+        <?php }  ?>
+
+    </div>
+
+</div>
+
+<div class="container">
+
+    <div class="starter-template">
+        <h1>Bootstrap starter template</h1>
+        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+    </div>
+
+</div><!-- /.container -->
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="catalog/view/javascript/bootstrap/docs/assets/js/vendor/jquery.min.js"><\/script>')</script>
+<script src="catalog/view/javascript/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="catalog/view/javascript/bootstrap/docs/assets/js/ie10-viewport-bug-workaround.js"></script>
+
+
+<!--megamenu start-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+<script>
+    window.Modernizr || document.write('<script src="libs/megamenu-js-master/megamenu-js-master/js/vendor/modernizr-2.8.3.min.js"><\/script>')
+</script>
+<script src="libs/megamenu-js-master/megamenu-js-master/js/megamenu.js"></script>
+<!--megamenu end-->
+<script src="catalog/js/mainpage.js"></script>
+
+
+</body>
+</html>
